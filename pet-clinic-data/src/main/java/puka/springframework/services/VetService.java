620d6a56f31eaa1c9ad7531795b63aa2101d51da
Pukazec <skvorc.leo@gmail.com>
2022-11-13 12:02:02 +0100

    Refactoring to CommonBase crud Interface. Closes #11.

--- pet-clinic-data/src/main/java/puka/springframework/services/VetService.java
+++ pet-clinic-data/src/main/java/puka/springframework/services/VetService.java
@@ -2,13 +2,5 @@ package puka.springframework.services;
 
 import puka.springframework.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
