b589f2c4ff26a704c66b44e4de7fa6e2f65d451c
Pukazec <skvorc.leo@gmail.com>
2022-11-13 12:21:35 +0100

    Implement Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/puka/springframework/services/map/OwnerServiceMap.java
@@ -0,0 +1,36 @@
+package puka.springframework.services.map;
+
+import org.springframework.stereotype.Service;
+import puka.springframework.model.Owner;
+import puka.springframework.services.CrudService;
+
+import java.util.Set;
+
+@Service
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
